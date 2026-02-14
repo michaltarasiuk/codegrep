0ec9acce24da9ced37f56a79366c458879f3a508
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-02-14 16:21:23 +0100

    add tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,18 @@
+import "@primer/react-brand/lib/css/main.css";
+
+import { AnimationProvider, ThemeProvider } from "@primer/react-brand";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
@@ -9,4 +22,12 @@ if (!rootElement) {
   );
 }
 
-createRoot(rootElement).render(<StrictMode />);
+createRoot(rootElement).render(
+  <StrictMode>
+    <ThemeProvider>
+      <AnimationProvider>
+        <RouterProvider router={router} />
+      </AnimationProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,13 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
