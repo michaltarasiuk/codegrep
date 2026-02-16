0f6dfeace82a4179a134db9f12d48b58fc6a0f92
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-02-16 12:26:48 +0100

    add user nav component

--- apps/web/src/lib/components/app-sidebar.svelte
+++ apps/web/src/lib/components/app-sidebar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
   import type { ComponentProps } from "svelte";
+  import NavUser from "./nav-user.svelte";
 
   let { ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
@@ -8,5 +9,13 @@
 <Sidebar.Root collapsible="offcanvas" {...restProps}>
   <Sidebar.Header />
   <Sidebar.Content />
-  <Sidebar.Footer />
+  <Sidebar.Footer>
+    <NavUser
+      user={{
+                name: "John Doe",
+                email: "john.doe@gmail.com",
+                avatar: "https://avatars.githubusercontent.com/u/69385846",
+            }}
+    />
+  </Sidebar.Footer>
 </Sidebar.Root>
