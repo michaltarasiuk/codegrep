b7ebfd311d89a7e7d989b6deab04aa69a6fb0016
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-02-18 13:10:58 +0100

    add chat composer

---
+++ apps/web/src/lib/components/chat-composer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import ArrowUpIcon from "@lucide/svelte/icons/arrow-up";
+  import * as InputGroup from "$lib/components/ui/input-group/index.js";
+  import { Separator } from "$lib/components/ui/separator/index.js";
+</script>
+
+<InputGroup.Root>
+  <InputGroup.Textarea placeholder="Ask, Search or Chat..." />
+  <InputGroup.Addon align="block-end">
+    <InputGroup.Text class="ms-auto">52% used</InputGroup.Text>
+    <Separator orientation="vertical" class="!h-4" />
+    <InputGroup.Button
+      variant="default"
+      class="rounded-full"
+      size="icon-xs"
+      disabled
+    >
+      <ArrowUpIcon />
+      <span class="sr-only">Send</span>
+    </InputGroup.Button>
+  </InputGroup.Addon>
+</InputGroup.Root>
