9f65c136f2410ca2257102edeaea8e8ace14de0f
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-02-18 11:22:47 +0100

    add base layout

--- apps/web/src/lib/components/app-sidebar.svelte
+++ apps/web/src/lib/components/app-sidebar.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-  import type { ComponentProps } from "svelte";
-  import NavUser from "./nav-user.svelte";
+  import UserMenu from "./user-menu.svelte";
   import ThemeToggle from "./theme-toggle.svelte";
-
-  let { ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
 </script>
 
-<Sidebar.Root collapsible="offcanvas" {...restProps}>
-  <Sidebar.Header />
-  <Sidebar.Content />
-  <Sidebar.Footer>
-    <NavUser
-      user={{
-        name: "John Doe",
-        email: "john.doe@gmail.com",
-        avatar: "https://avatars.githubusercontent.com/u/69385846",
-      }}
-    />
-    <ThemeToggle />
-  </Sidebar.Footer>
-</Sidebar.Root>
+<Sidebar.Provider
+  style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+>
+  <Sidebar.Root collapsible="offcanvas" variant="inset">
+    <Sidebar.Header />
+    <Sidebar.Content />
+    <Sidebar.Footer>
+      <UserMenu
+        user={{
+          name: "John Doe",
+          email: "john.doe@gmail.com",
+          avatar: "https://avatars.githubusercontent.com/u/69385846",
+        }}
+      />
+      <ThemeToggle />
+    </Sidebar.Footer>
+  </Sidebar.Root>
+  <Sidebar.Inset>
+    <header
+      class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
+    >
+      <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+        <Sidebar.Trigger class="-ms-1" />
+      </div>
+    </header>
+    <slot />
+  </Sidebar.Inset>
+</Sidebar.Provider>
