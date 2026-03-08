2d75bbf35fbec98158a821e175c546fdc6791ea1
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-03-08 23:23:41 +0100

    update web tsconfig

--- apps/web/src/routes/chat/[id]/+page.server.ts
+++ apps/web/src/routes/chat/[id]/+page.server.ts
@@ -1,15 +1,9 @@
-import { error } from "@sveltejs/kit";
-
 import { client } from "$lib/utils/client";
 
 export const prerender = false;
 
 export async function load({ params, request }) {
   const chatId = params.id;
-
-  if (!chatId) {
-    error(404, "Chat not found");
-  }
 
   const messages = await client.api
     .chat({
