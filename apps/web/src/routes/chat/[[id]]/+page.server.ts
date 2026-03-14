371e843a1f61307eebfc83041e11945f56c59091
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-03-14 23:53:02 +0100

    refactor chat loader

--- apps/web/src/routes/chat/[[id]]/+page.server.ts
+++ apps/web/src/routes/chat/[[id]]/+page.server.ts
@@ -1,4 +1,4 @@
-import type { UIMessage } from "ai";
+import { error } from "@sveltejs/kit";
 
 import { client } from "$lib/utils/client";
 import { isDefined } from "$lib/utils/is-defined.js";
@@ -6,17 +6,23 @@ import { isDefined } from "$lib/utils/is-defined.js";
 import type { PageServerLoadEvent } from "./$types";
 
 export async function load({ params, request }: PageServerLoadEvent) {
-  let messages: UIMessage[] = [];
-  if (isDefined(params.id)) {
-    const chat = client.api.chat({
-      id: params.id,
-    });
-    const persistedMessages = await chat.messages.get({
-      headers: Object.fromEntries(request.headers),
-    });
-    messages = persistedMessages.data ?? [];
+  if (!isDefined(params.id)) {
+    return;
   }
-  return {
-    messages,
-  };
+  const chat = client.api.chat({
+    id: params.id,
+  });
+  const persistedMessages = await chat.messages.get({
+    headers: Object.fromEntries(request.headers),
+  });
+  if (!persistedMessages.error) {
+    return {
+      messages: persistedMessages.data,
+    };
+  }
+  let status = "Failed to load chat";
+  if (persistedMessages.error.status === 404) {
+    status = "Chat not found";
+  }
+  throw error(persistedMessages.error.status, status);
 }
