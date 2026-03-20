18a6e82da8b568ad947496b05526d2ff629d8641
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-03-20 08:17:24 +0100

    add checkbox

---
+++ apps/web/src/lib/components/ui/checkbox/checkbox.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import CheckIcon from "@lucide/svelte/icons/check";
+  import MinusIcon from "@lucide/svelte/icons/minus";
+  import { Checkbox as CheckboxPrimitive } from "bits-ui";
+
+  import { cn, type WithoutChildrenOrChild } from "$lib/utils/cn.js";
+
+  let {
+    ref = $bindable(null),
+    checked = $bindable(false),
+    indeterminate = $bindable(false),
+    class: className,
+    ...restProps
+  }: WithoutChildrenOrChild<CheckboxPrimitive.RootProps> = $props();
+</script>
+
+<CheckboxPrimitive.Root
+  bind:ref
+  bind:checked
+  bind:indeterminate
+  data-slot="checkbox"
+  class={cn(
+    "border-input dark:bg-input/30 data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary data-checked:border-primary aria-invalid:aria-checked:border-primary aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 peer relative flex size-4 shrink-0 items-center justify-center rounded-lg border transition-colors outline-none group-has-disabled/field:opacity-50 after:absolute after:-inset-x-3 after:-inset-y-2 focus-visible:ring-3 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:ring-3",
+    className
+  )}
+  {...restProps}
+>
+  {#snippet children({ checked, indeterminate })}
+    <div
+      data-slot="checkbox-indicator"
+      class="grid place-content-center text-current transition-none [&>svg]:size-3.5"
+    >
+      {#if checked}
+        <CheckIcon />
+      {:else if indeterminate}
+        <MinusIcon />
+      {/if}
+    </div>
+  {/snippet}
+</CheckboxPrimitive.Root>
