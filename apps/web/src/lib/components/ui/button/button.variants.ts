bb48ab52a17ba5d60ede1caf1295729815d9f733
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-03-24 20:24:45 +0100

    update tokens

--- apps/web/src/lib/components/ui/button/button.variants.ts
+++ apps/web/src/lib/components/ui/button/button.variants.ts
@@ -9,7 +9,7 @@ export const buttonVariants = tv({
       destructive:
         "bg-destructive hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40 text-white shadow-xs",
       outline:
-        "bg-background hover:bg-accent hover:text-accent-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50 border shadow-xs",
+        "bg-background hover:bg-accent hover:text-accent-foreground dark:border-input dark:bg-input-muted dark:hover:bg-input-muted-hover border shadow-xs",
       secondary:
         "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-xs",
       ghost:
