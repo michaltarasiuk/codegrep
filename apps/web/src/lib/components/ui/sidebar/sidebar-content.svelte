ff444f2b6f6d40b613246a91607a0f3844c673fd
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-04-09 22:33:57 +0200

    use ScrollShadow component in Sidebar

--- apps/web/src/lib/components/ui/sidebar/sidebar-content.svelte
+++ apps/web/src/lib/components/ui/sidebar/sidebar-content.svelte
@@ -1,25 +1,24 @@
 <script lang="ts">
   import type { HTMLAttributes } from "svelte/elements";
 
+  import { ScrollShadow } from "$lib/components/scroll-shadow";
   import { cn, type WithElementRef } from "$lib/utils/cn.js";
 
   let {
-    ref = $bindable(null),
     children,
     class: className,
     ...restProps
-  }: WithElementRef<HTMLAttributes<HTMLElement>> = $props();
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
-<div
-  bind:this={ref}
+<ScrollShadow
   data-slot="sidebar-content"
   data-sidebar="content"
   class={cn(
-    "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+    "scrollbar-hide flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
     className
   )}
   {...restProps}
 >
   {@render children?.()}
-</div>
+</ScrollShadow>
