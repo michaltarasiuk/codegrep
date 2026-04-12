564cacb53d1b69673e07c173267ebb2a6198f109
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-04-12 22:40:25 +0200

    naming refactor

--- apps/web/src/lib/components/user-account-menu.svelte
+++ apps/web/src/lib/components/user-account-menu.svelte
@@ -21,7 +21,7 @@
   let hydrated = $state(false);
   let personalInstructionsOpen = $state(false);
 
-  onMount(function markUserAccountMenuHydrated() {
+  onMount(function hydrate() {
     hydrated = true;
   });
 
