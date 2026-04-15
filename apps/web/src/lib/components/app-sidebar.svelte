dca3a8b717ab981fac0c93b6c7f1993c82c0442d
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-04-15 16:27:15 +0200

    update typescript configuration

--- apps/web/src/lib/components/app-sidebar.svelte
+++ apps/web/src/lib/components/app-sidebar.svelte
@@ -65,7 +65,7 @@
   </Sidebar.Root>
   <Sidebar.Inset>
     <header
-      class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
+      class="flex h-(--header-height) shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
     >
       <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <Sidebar.Trigger class="-ms-1" />
