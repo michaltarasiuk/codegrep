e39ad4c827e84ed5fcef30dad07b93f91f00376d
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2026-04-15 20:34:55 +0200

    update file structure

---
+++ packages/fetch-proxy/src/index.ts
@@ -0,0 +1,12 @@
+/*!---------------------------------------------------------------------------------------------
+ * Copyright (c) 2023 Michael Jackson. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *
+ * Ported from https://github.com/mjackson/remix-the-web/tree/main/packages/fetch-proxy
+ *--------------------------------------------------------------------------------------------*/
+
+export {
+  createFetchProxy,
+  type FetchProxy,
+  type FetchProxyOptions,
+} from "./fetch-proxy.js";
